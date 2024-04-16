f54d0cecd4440f5218bb09fd62e97d539956ff2a
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-04-16 17:17:07 -0400

    feat: new auth docs ia

--- apps/www/lib/redirects.js
+++ apps/www/lib/redirects.js
@@ -2433,4 +2433,9 @@ module.exports = [
     source: '/special-announcement/tickets/:path*',
     destination: '/ga-week/tickets/:path*',
   },
+  {
+    permanent: true,
+    source: '/docs/guides/auth/sessions',
+    destination: '/docs/guides/auth/concepts/sessions',
+  },
 ]
